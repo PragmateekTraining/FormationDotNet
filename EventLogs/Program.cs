ecc495984eb8565c032748d0b3e0d5d2f7c7db39
Pragmateek <postmaster@pragmateek.com>
2013-09-22 01:30:47 +0200

    Windows event log and registry samples

---
+++ EventLogs/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Security;
+
+namespace EventLogs
+{
+    class Program
+    {
+        const string SourceName = "NightlyBatch";
+
+        static void Main(string[] args)
+        {
+            if (args.Length >= 1)
+            {
+                if (args[0] == "--create")
+                {
+                    if (EventLog.SourceExists(SourceName))
+                    {
+                        Console.Error.WriteLine("Source '{0}' already exists!", SourceName);
+                        return;
+                    }
+
+                    try
+                    {
+                        EventLog.CreateEventSource(SourceName, string.Format("Creating new source for '{0}'...", SourceName));
+                    }
+                    catch (SecurityException)
+                    {
+                        Console.Error.WriteLine("To create the source you must run this program as administrator!");
+                        return;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.Error.WriteLine("Caught an unexpected exception: {0}!", e);
+                        return;
+                    }
+
+                    Console.WriteLine("Source '{0}' created.", SourceName);
+
+                    return;
+                }
+                else if (args[0] == "--delete")
+                {
+                    if (!EventLog.SourceExists(SourceName))
+                    {
+                        Console.Error.WriteLine("Source '{0}' does not exist!", SourceName);
+                        return;
+                    }
+
+                    try
+                    {
+                        EventLog.DeleteEventSource(SourceName);
+                    }
+                    catch (SecurityException)
+                    {
+                        Console.Error.WriteLine("To delete the source you must run this program as administrator!");
+                        return;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.Error.WriteLine("Caught an unexpected exception: {0}!", e);
+                        return;
+                    }
+
+                    Console.WriteLine("Source '{0}' deleted.", SourceName);
+
+                    return;
+                }
+                else if (args[0] == "--monitor")
+                {
+                    if (!EventLog.SourceExists(SourceName))
+                    {
+                        Console.Error.WriteLine("Source '{0}' does not exist!", SourceName);
+                        return;
+                    }
+
+                    EventLog log = new EventLog("Application", ".", SourceName);
+                    log.EnableRaisingEvents = true;
+                    log.EntryWritten += (s, a) =>
+                        {
+                            Console.WriteLine("New entry: '{0}'.", a.Entry.ReplacementStrings[0]);
+                        };
+
+                    Console.WriteLine("Press enter to stop monitoring.");
+                    Console.ReadLine();
+                    Console.Write("Monitoring stopped.");
+
+                    return;
+                }
+            }
+
+            while (true)
+            {
+                Console.Write("Enter some message: ");
+
+                string message = Console.ReadLine();
+
+                if (message == "") break;
+
+                EventLog.WriteEntry(SourceName, message, EventLogEntryType.Error, 1);
+            }
+        }
+    }
+}
---
+++ Registry/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+
+namespace RegistryAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (RegistryKey NDP = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
+            {
+                foreach (string versionName in NDP.GetSubKeyNames().Where(skn => skn.StartsWith("v")))
+                {
+                    using (RegistryKey versionKey = NDP.OpenSubKey(versionName))
+                    {
+                        if (versionKey.GetValue(null) as string == "deprecated") continue;
+
+                        string installPath = versionKey.GetValue("InstallPath") as string;
+
+                        if (installPath == null)
+                        {
+                            RegistryKey fullKey = versionKey.OpenSubKey("Full");
+
+                            if (fullKey != null)
+                            {
+                                installPath = fullKey.GetValue("InstallPath") as string;
+                            }
+                        }
+
+                        Console.Write(versionName);
+
+                        if (installPath != null)
+                            Console.Write(" ({0})", installPath);
+
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
