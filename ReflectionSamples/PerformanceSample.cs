598f2044982084a5dad5361501c52f13c3becf8e
Pragmateek <postmaster@pragmateek.com>
2013-09-28 13:06:11 +0200

    Reflection performance sample

---
+++ ReflectionSamples/PerformanceSample.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using SamplesAPI;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace ReflectionSamples
+{
+    public class PerformanceSample : ISample
+    {
+        public static void F()
+        {
+        }
+
+        private long n;
+
+        public PerformanceSample(long n)
+        {
+            this.n = n;
+        }
+
+        public void Run()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            for (int i = 1; i <= n; ++i)
+            {
+                F();
+            }
+            stopwatch.Stop();
+
+            TimeSpan t1 = stopwatch.Elapsed;
+
+            MethodInfo method = typeof(PerformanceSample).GetMethod("F", BindingFlags.Public | BindingFlags.Static);
+
+            stopwatch.Restart();
+            for (int i = 1; i <= n; ++i)
+            {
+                method.Invoke(null, null);
+            }
+            stopwatch.Stop();
+
+            TimeSpan t2 = stopwatch.Elapsed;
+
+            long ratio = t2.Ticks / t1.Ticks;
+
+            Console.WriteLine("Without reflection: {0}", t1);
+            Console.WriteLine("With reflection: {0} (x{1})", t2, ratio);
+        }
+    }
+}
---
+++ ReflectionSamples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReflectionSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                if (args[0] == "performance")
+                {
+                    long n = Convert.ToInt64(args[1]);
+
+                    new PerformanceSample(n).Run();
+                }
+            }
+        }
+    }
+}
