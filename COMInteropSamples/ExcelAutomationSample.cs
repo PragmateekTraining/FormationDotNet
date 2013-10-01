996fbbe416c4490a3092851de23bbc8378fb5c10
Pragmateek <postmaster@pragmateek.com>
2013-10-01 15:16:42 +0200

    Starting COM interop samples

---
+++ COMInteropSamples/ExcelAutomationSample.cs
@@ -0,0 +1,15 @@
+﻿using SamplesAPI;
+using System;
+
+namespace COMInteropSamples
+{
+    public class ExcelAutomationSample : ISample
+    {
+        public void Run()
+        {
+            Type type = Type.GetTypeFromProgID("Excel.Application");
+            dynamic excel = Activator.CreateInstance(type);
+            excel.Visible = true;
+        }
+    }
+}
