0c90707fa8e5be256bf86389e6c6c14e0117774a
Pragmateek <postmaster@pragmateek.com>
2013-10-13 17:09:58 +0200

    Lazy standard basic sample

---
+++ LazySamples/StandardImplementationSample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LazySamples
+{
+    class StandardImplementationSample
+    {
+        private Lazy<object> lazyObject = new Lazy<object>(() => new object());
+
+        public object Object
+        {
+            get
+            {
+                return lazyObject.Value;
+            }
+        }
+    }
+}
