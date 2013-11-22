0e5ab274bba78fb3ab44c7d676b752aaf0945480
Pragmateek <postmaster@pragmateek.com>
2013-11-22 21:48:35 +0100

    ...

---
+++ BinarySerialization/build_library.bat
@@ -0,0 +1,5 @@
+CALL "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\vsvars32.bat"
+
+csc /target:library B.cs
+csc /reference:B.dll /target:library A.cs
+csc /target:library /out:C.dll B.cs
