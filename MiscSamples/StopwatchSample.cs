5f789708a7a004cdcca62318fe03d169c39d028b
Pragmateek <postmaster@pragmateek.com>
2013-11-22 01:18:46 +0100

    Stopwatch overhead

--- MiscSamples/StopwatchSample.cs
+++ MiscSamples/StopwatchSample.cs
@@ -8,8 +8,16 @@ namespace MiscSamples
     {
         public void Run()
         {
+            // Dump local stopwatch technical specifications.
             Console.WriteLine("Is high resolution? {0}", Stopwatch.IsHighResolution);
             Console.WriteLine("Frequency? {0}", Stopwatch.Frequency);
+
+            // Measure start/stop overhead.
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            stopwatch.Stop();
+            TimeSpan elapsed = stopwatch.Elapsed;
+            Console.WriteLine("Elapsed: {0}", elapsed);
         }
     }
 }
--- ValueTypes/Program.cs
+++ ValueTypes/Program.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
             // new BulkAllocationSample().Run();
-            // new EqualsOverloadSample(1000000).Run();
-            new DefaultComparisonSample().Run();
+            new EqualsOverloadSample(10000000).Run();
+            // new DefaultComparisonSample().Run();
             // new BasicSample().Run();
             // new BoxingPerformanceImpactSample().Run();
         }
